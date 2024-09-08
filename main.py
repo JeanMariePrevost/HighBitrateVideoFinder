ca5ec2d343add77206fafb419257319f99865016
Jean-Marie Prevost <jeanmarie.prevost@outlook.com>
2024-09-08 13:29:31 -0400

    Clean up video_file.py Use partial CRC32 for added file recognition in case of filename issues or collisions (other hashes were too slow for the minimal benefits for this use case)

--- main.py
+++ main.py
@@ -26,27 +26,7 @@ if __name__ == "__main__":
     end_time = datetime.datetime.now()
     print(f"Time needed to get the size of all video files: {end_time - start_time}")
     print("#" * 20)
-    
-    #DEBUG: calculate time needed to get the duration of all video files
-    print("#" * 20)
-    print("Calculating the duration of all video files via ffmpeg...")
-    start_time = datetime.datetime.now()
-    for video_file in video_files:
-        print(video_file.get_duration_ffmpeg())
-    end_time = datetime.datetime.now()
-    print(f"Time needed to get the duration of all video files: {end_time - start_time}")
-    print("#" * 20)
-    
-    #DEBUG: calculate time needed to get the duration of all video files
-    print("#" * 20)
-    print("Calculating the duration of all video files via mediainfo...")
-    start_time = datetime.datetime.now()
-    for video_file in video_files:
-        print(video_file.get_duration_mediainfo())
-    end_time = datetime.datetime.now()
-    print(f"Time needed to get the duration of all video files: {end_time - start_time}")
-    print("#" * 20)
-    
+
     #DEBUG: calculate time needed to get the duration of all video files
     print("#" * 20)
     print("Calculating the duration of all video files...")
@@ -58,25 +38,24 @@ if __name__ == "__main__":
     print("#" * 20)
     
     #DEBUG: calculate time needed to get the bitrate of all video files
-    # print("#" * 20)
-    # print("Calculating the bitrate of all video files...")
-    # start_time = datetime.datetime.now()
-    # for video_file in video_files:
-    #     print(video_file.get_bitrate())
-    # end_time = datetime.datetime.now()
-    # print(f"Time needed to get the bitrate of all video files: {end_time - start_time}")
-    # print("#" * 20)
+    print("#" * 20)
+    print("Calculating the bitrate of all video files...")
+    start_time = datetime.datetime.now()
+    for video_file in video_files:
+        print(video_file.get_bitrate())
+    end_time = datetime.datetime.now()
+    print(f"Time needed to get the bitrate of all video files: {end_time - start_time}")
+    print("#" * 20)
     
-    #DEBUG: calculate time needed to get the hash of all video files
-    # print("#" * 20)
-    # print("Calculating the hash of all video files...")
-    # start_time = datetime.datetime.now()
-    # for video_file in video_files:
-    #     print(video_file.compute_file_hash())
-    # end_time = datetime.datetime.now()
-    # print(f"Time needed to get the hash of all video files: {end_time - start_time}")
-    # print("#" * 20)
-    
+    #DEBUG: calculate time needed to get the partial crc32 of all video files
+    print("#" * 20)
+    print("Calculating the partial crc32 of all video files...")
+    start_time = datetime.datetime.now()
+    for video_file in video_files:
+        print(video_file.compute_partial_file_crc32())
+    end_time = datetime.datetime.now()
+    print(f"Time needed to get the partial crc32 of all video files: {end_time - start_time}")
+    print("#" * 20)
     
     
     # Define a name and path for the CSV file, using a timestamp
@@ -86,4 +65,6 @@ if __name__ == "__main__":
     # save_to_csv(video_files, output_file)
 
     # print(f"\nVideo files list saved to {output_file}.")
+    
+    
     
--- video_file.py
+++ video_file.py
@@ -1,93 +1,52 @@
 from functools import lru_cache
-import os
-import hashlib
-import ffmpeg
-from moviepy.editor import VideoFileClip
 from pymediainfo import MediaInfo
+import os
+import zlib
 
 class VideoFile:
     """
-    Class representing a video file with its attributes.
-    Also handles everything related to the video file, like figuring out its bitrate, length, hash...
+    A class to represent a video file.
+    Handles various video file operations such as getting the duration, file size, and bitrate.
     
     Example usage:
-    video_file = VideoFile('path/to/video.mp4')
-    print(video_file.size)  # File size in bytes
-    print(video_file.compute_file_hash())  # Get the file hash
+    video_file = VideoFile("path_to_your_video.mp4")
+    duration = video_file.get_duration()
     """
     
     def __init__(self, file_path):
         self.file_path = file_path
 
-
-    @lru_cache(maxsize=None)
-    def get_clip(self):
-        return VideoFileClip(self.file_path)
-    
     @lru_cache(maxsize=None)
     def get_duration(self):
-        return self.get_clip().duration  # Duration in seconds
-    
+        """Returns the duration of the video in seconds."""
+        media_info = MediaInfo.parse(self.file_path)
+        for track in media_info.tracks:
+            if track.track_type == 'Video':
+                duration_in_ms = track.duration
+                if duration_in_ms is not None:
+                    return int(duration_in_ms) / 1000
+        return None
+
     @lru_cache(maxsize=None)
     def get_filesize(self):
-        return os.path.getsize(self.file_path)  # File size in bytes
-    
+        """Returns the file size in bytes."""
+        return os.path.getsize(self.file_path)
+
     @lru_cache(maxsize=None)
     def get_bitrate(self):
-        return (self.get_filesize() * 8) / self.get_duration()  # Bitrate in bits per second
-    
-    
-    
-
-    @lru_cache(maxsize=None)
-    def get_duration_ffmpeg(self):
-        try:
-            probe = ffmpeg.probe(self.file_path)
-            video_streams = [stream for stream in probe['streams'] if stream['codec_type'] == 'video']
-            if video_streams:
-                video_stream = video_streams[0]
-                # Check if 'duration' key exists in the video stream dictionary
-                if 'duration' in video_stream:
-                    duration = float(video_stream['duration'])
-                    return duration
-                else:
-                    print("Duration key not found in video stream metadata.")
-            else:
-                print("No video streams found in the file.")
-        except ffmpeg.Error as e:
-            print("An error occurred while probing the file:", e)
+        """Calculates and returns the bitrate of the video in bits per second."""
+        duration = self.get_duration()
+        if duration is not None and duration > 0:
+            filesize = self.get_filesize()  # in bytes
+            return (filesize * 8) / duration
         return None
 
     @lru_cache(maxsize=None)
-    def get_duration_mediainfo(self):
-        try:
-            media_info = MediaInfo.parse(self.file_path)
-            for track in media_info.tracks:
-                if track.track_type == 'Video':
-                    duration_in_ms = track.duration
-                    if duration_in_ms is not None:
-                        duration_in_seconds = int(duration_in_ms) / 1000
-                        return duration_in_seconds
-            print("No Video track found in file.")
-        except Exception as e:
-            print("An error occurred while reading the file:", e)
-        return None
-
-    @lru_cache(maxsize=None)
-    def compute_file_hash(self, hash_algorithm='sha256'):
-        """Compute a hash for the video file using its content."""
-        hash_func = hashlib.new(hash_algorithm)
+    def compute_partial_file_crc32(self, block_size=1048576 * 8):  # 8 MB by default
+        """Computes the CRC32 hash of only the first block_size bytes of the video file."""
+        file_hash = 0
         with open(self.file_path, 'rb') as f:
-            while chunk := f.read(8192):  # Read file in chunks
-                hash_func.update(chunk)
-        return hash_func.hexdigest()
-
-    def to_dict(self):
-        """Convert the object to a dictionary for easy CSV export."""
-        return {
-            'Video File Path': self.file_path,
-            'File Size (Bytes)': self.size,
-            'File Hash': self.hash,
-        }
-
-
+            fb = f.read(block_size)  # Read only the first block_size bytes
+            file_hash = zlib.crc32(fb, file_hash)  # Compute CRC32 for the read portion
+        
+        return format(file_hash & 0xFFFFFFFF, '08x')
