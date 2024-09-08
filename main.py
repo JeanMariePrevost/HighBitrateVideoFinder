a460154d2878afb93d9e01ab2b4867a08416e4bb
Jean-Marie Prevost <jeanmarie.prevost@outlook.com>
2024-09-08 14:27:04 -0400

    Refactor video file processing and add progress display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
 from save_to_csv import save_to_csv
 from video_file_scanner import scan_directory
+from tqdm import tqdm
 
 
 
@@ -14,15 +14,32 @@ if __name__ == "__main__":
     #Print results overview
     print(f"\nFound {len(video_files)} video files in [{directory}]")
     
+    
+    # Extract/compute (and cache) the metadata
+    print("\nProcessing video files...")
+    for video_file in tqdm(video_files, unit="file"):
+        video_file.get_filesize()
+    
+    print("\nComputing partial CRC32...")
+    for video_file in tqdm(video_files, unit="file"):
+        video_file.compute_partial_file_crc32()
+    
+    print("\nClaculating bitrates... (This is the slowest step)")
+    for video_file in tqdm(video_files, unit="file"):
+        video_file.get_bitrate()
+    
+    
     #sort the video files by bitrate
+    print("\nSorting video files by bitrate...")
     video_files.sort(key=lambda x: x.get_bitrate(), reverse=True)
         
     # Save the results list to the CSV
+    print("\nSaving results to CSV...")
     output_file = save_to_csv(video_files)
 
-    print("Scan complete.")
-    print(f"\n{len(video_files)} video files scanned and processed.")
-    print(f"\nVideo files list saved to {output_file}.")
+    print("\n\nScan complete.")
+    print(f"{len(video_files)} video files scanned and processed.")
+    print(f"Video files list saved to {output_file}.")
     input("Press Enter to exit...")
     exit(0)
     
